1349ad68124089c21424bf1d5f1e4fd8924ad525
SabrinaFaustino <rm99570@fiap.com.br>
2023-11-24 02:59:34 -0300

    Inserções da tabela casos e doencas adicionados

--- SCRIPT_DML.sql
+++ SCRIPT_DML.sql
@@ -112,48 +112,134 @@ INSERT INTO T_VGS_UNIDADE (
 
 
 ------------------------------------------
---Insere os medicos na coluna T_VGS_MEDICO 
+--Insere os medicos na tabela T_VGS_MEDICO 
 
 INSERT INTO T_VGS_MEDICO (nom_medico, des_especialidade, num_crm, des_telefone, id_unidade, des_email, des_senha) 
     VALUES ('Dr. Jo�o', 'Infectologia', 'SP123456', '11987654321', 1, 'joao@gmail.com', '123456joao');
+
 INSERT INTO T_VGS_MEDICO (nom_medico, des_especialidade, num_crm, des_telefone,id_unidade, des_email, des_senha) 
     VALUES ( 'Dr. Ana', 'Ginecologia', 'RJ789012', '21987654321', 2, 'ana@hotmail.com', 'senhaAna789');
+
 INSERT INTO T_VGS_MEDICO (nom_medico, des_especialidade, num_crm, des_telefone, id_unidade, des_email, des_senha) 
     VALUES ('Dr. Carlos', 'Cardiologia', 'PE345678', '81987654321', 4, 'carlos@email.com', 'senhaCarlos987');
+
 INSERT INTO T_VGS_MEDICO (nom_medico, des_especialidade, num_crm, des_telefone,id_unidade, des_email, des_senha)
     VALUES ('Dra. Fernanda', 'Pediatria', 'SP567890', '11976543210', 9, 'fernanda@yahoo.com', 'senhaFernanda543');
+
 INSERT INTO T_VGS_MEDICO ( nom_medico, des_especialidade, num_crm, des_telefone, id_unidade, des_email, des_senha) 
     VALUES ('Dr. Marcos', 'Ortopedia', 'DF123789', '61976543210', 1, 'marcos@hotmail.com', 'senhaMarcos321');
+
 INSERT INTO T_VGS_MEDICO (nom_medico, des_especialidade, num_crm, des_telefone, id_unidade, des_email, des_senha)
     VALUES ('Dra. Juliana', 'Dermatologia', 'MG987654', '31987654321', 5, 'juliana@gmail.com', 'senhaJuliana654');
+
 INSERT INTO T_VGS_MEDICO (nom_medico, des_especialidade, num_crm, des_telefone, id_unidade, des_email, des_senha) 
     VALUES ('Dr. Rafael', 'Neurologia', 'BA234567', '71987654321', 7, 'rafael@yahoo.com', 'senhaRafael876');
+
 INSERT INTO T_VGS_MEDICO (nom_medico, des_especialidade, num_crm, des_telefone, id_unidade, des_email, des_senha) 
     VALUES ('Dra. Isabella', 'Oftalmologia', 'SC456789', '47987654321', 6, 'isabella@gmail.com', 'senhaIsabella789');
+
 INSERT INTO T_VGS_MEDICO (nom_medico, des_especialidade, num_crm, des_telefone, id_unidade, des_email, des_senha) 
     VALUES ('Dra. Camila', 'Psiquiatria', 'SP987654', '11987654321', 6, 'camila@hotmail.com', 'senhaCamila987');
 INSERT INTO T_VGS_MEDICO ( nom_medico, des_especialidade, num_crm, des_telefone, id_unidade, des_email, des_senha) 
     VALUES ('Dr. Guilherme', 'Otorrinolaringologia', 'RJ123789', '21976543210', 7, 'guilherme@hotmail.com', 'senhaGuilherme321');
+
 INSERT INTO T_VGS_MEDICO (nom_medico, des_especialidade, num_crm, des_telefone, id_unidade, des_email, des_senha) 
     VALUES ('Dra. Eduardo', 'Urologia', 'PE345678', '81987654321', 2, 'eduardo@yahoo.com', 'senhaEduardo543');
+
 INSERT INTO T_VGS_MEDICO (nom_medico, des_especialidade, num_crm, des_telefone, id_unidade, des_email, des_senha) 
     VALUES ('Dr. Gabriela', 'Gastroenterologia', 'DF567890', '61976543210', 4, 'gabriela@outlook.com', 'senhaGabriela654');
+
 INSERT INTO T_VGS_MEDICO (nom_medico, des_especialidade, num_crm, des_telefone, id_unidade, des_email, des_senha) 
     VALUES ('Dra. Lucas', 'Endocrinologia', 'MG123789', '31987654321', 5, 'lucas@gmail.com', 'senhaLucas321');
+
 INSERT INTO T_VGS_MEDICO (nom_medico, des_especialidade, num_crm, des_telefone, des_email, des_senha, id_unidade)
     VALUES ('Dr. Ana Silva', 'Cardiologia', 'SP445567', '11944522335', 'ana.silva@gmail.com', 'senhaanasilva', 9);
+
 INSERT INTO T_VGS_MEDICO (nom_medico, des_especialidade, num_crm, des_telefone, des_email, des_senha, id_unidade)
     VALUES ('Dr. Marcos Oliveira', 'Pediatria', 'SP754321', '11965876587', 'marcos.oliveira@hotmail.com', 'marcospediatra', 8);
+
 INSERT INTO T_VGS_MEDICO (nom_medico, des_especialidade, num_crm, des_telefone, des_email, des_senha, id_unidade)
     VALUES ('Dra. Renata Santos', 'Dermatologia', 'SP976543', '11976554433', 'renata.santos@yahoo.com', 'senharenata789', 1);
+
 INSERT INTO T_VGS_MEDICO (nom_medico, des_especialidade, num_crm, des_telefone, des_email, des_senha, id_unidade)
     VALUES ('Dr. Rafael Pereira', 'Ortopedia', 'SP765432', '11999532143', 'rafael.pereira@hotmail.com', 'senhapereira101', 9);
+
 INSERT INTO T_VGS_MEDICO (nom_medico, des_especialidade, num_crm, des_telefone, des_email, des_senha, id_unidade)
     VALUES ('Dra. Camila Lima', 'Ginecologia', 'SP345678', '11996677800', 'camila.lima@gmail.com', 'senhaLima112', 1);
+
 INSERT INTO T_VGS_MEDICO (nom_medico, des_especialidade, num_crm, des_telefone, des_email, des_senha, id_unidade)
     VALUES ('Dr. Jo�o Silva', 'Infectologia', 'SP468352', '1143425235', 'joao.silva@outlook.com', 'senha123', 1);
+
 INSERT INTO T_VGS_MEDICO (nom_medico, des_especialidade, num_crm, des_telefone, des_email, des_senha, id_unidade)
     VALUES ('Dra. Maria Oliveira', 'Infectologia', 'SP219843', '9976568785', 'maria.oliveira@outlook.com', 'senha456', 8);
+
+
+------------------------------------------
+--Insere as doencas na tabela T_VGS_DOENCA
+
+INSERT INTO T_VGS_DOENCA (nom_doenca, cod_cid_doenca, des_sintomas, num_taxa_transmissao, num_taxa_letalidade)
+    VALUES ('Gripe', 'A01', 'Febre, tosse, dor de cabeça', 2, 1);
+
+INSERT INTO T_VGS_DOENCA (nom_doenca, cod_cid_doenca, des_sintomas, num_taxa_transmissao, num_taxa_letalidade)
+    VALUES ('Resfriado', 'J00', 'Espirros, congestão nasal', 1, 0);
+
+INSERT INTO T_VGS_DOENCA (nom_doenca, cod_cid_doenca, des_sintomas, num_taxa_transmissao, num_taxa_letalidade)
+    VALUES ('COVID-19', 'U07', 'Febre, falta de ar, tosse seca', 3, 2);
+
+INSERT INTO T_VGS_DOENCA (nom_doenca, cod_cid_doenca, des_sintomas, num_taxa_transmissao, num_taxa_letalidade)
+    VALUES ('Dengue', 'A90', 'Febre, dor muscular, erupções cutâneas', 4, 1);
+
+INSERT INTO T_VGS_DOENCA (nom_doenca, cod_cid_doenca, des_sintomas, num_taxa_transmissao, num_taxa_letalidade)
+    VALUES ('Influenza', 'J10', 'Febre, dor de garganta, fadiga', 2, 1);
+
+INSERT INTO T_VGS_DOENCA (nom_doenca, cod_cid_doenca, des_sintomas, num_taxa_transmissao, num_taxa_letalidade)
+    VALUES ('H1N1', 'J09', 'Febre, tosse, dor no corpo', 3, 2);
+
+INSERT INTO T_VGS_DOENCA (nom_doenca, cod_cid_doenca, des_sintomas, num_taxa_transmissao, num_taxa_letalidade)
+    VALUES ('Zika', 'A92', 'Febre, erupções cutâneas, dor nas articulações', 3, 0.5);
+
+INSERT INTO T_VGS_DOENCA (nom_doenca, cod_cid_doenca, des_sintomas, num_taxa_transmissao, num_taxa_letalidade)
+    VALUES ('Sarampo', 'B05', 'Manchas vermelhas, febre, tosse', 5, 2);
+
+INSERT INTO T_VGS_DOENCA (nom_doenca, cod_cid_doenca, des_sintomas, num_taxa_transmissao, num_taxa_letalidade)
+    VALUES ('Varicela', 'B01', 'Erupções cutâneas, coceira, febre', 4, 0.5);
+
+INSERT INTO T_VGS_DOENCA (nom_doenca, cod_cid_doenca, des_sintomas, num_taxa_transmissao, num_taxa_letalidade)
+    VALUES ('Tuberculose', 'A15', 'Tosse persistente, perda de peso, fadiga', 1, 5);
+
+
+------------------------------------------
+--Insere os medicos na coluna T_VGS_MEDICO 
+
+INSERT INTO T_VGS_CASOS (dat_nasc_paciente, des_genero_paciente, dat_diagnostico, des_estado_diagnostico, id_doenca)
+    VALUES (TO_DATE('1990-01-15', 'YYYY-MM-DD'), 'Masculino', TO_DATE('2022-03-01', 'YYYY-MM-DD'), 'SP', 1);
+
+INSERT INTO T_VGS_CASOS (dat_nasc_paciente, des_genero_paciente, dat_diagnostico, des_estado_diagnostico, id_doenca)
+    VALUES (TO_DATE('1985-05-20', 'YYYY-MM-DD'), 'Feminino', TO_DATE('2022-03-05', 'YYYY-MM-DD'), 'RJ', 2);
+
+INSERT INTO T_VGS_CASOS (dat_nasc_paciente, des_genero_paciente, dat_diagnostico, des_estado_diagnostico, id_doenca)
+    VALUES (TO_DATE('1995-08-10', 'YYYY-MM-DD'), 'Masculino', TO_DATE('2022-03-10', 'YYYY-MM-DD'), 'MG', 3);
+
+INSERT INTO T_VGS_CASOS (dat_nasc_paciente, des_genero_paciente, dat_diagnostico, des_estado_diagnostico, id_doenca)
+    VALUES (TO_DATE('1980-06-25', 'YYYY-MM-DD'), 'Feminino', TO_DATE('2022-03-15', 'YYYY-MM-DD'), 'RS', 4);
+
+INSERT INTO T_VGS_CASOS (dat_nasc_paciente, des_genero_paciente, dat_diagnostico, des_estado_diagnostico, id_doenca)
+    VALUES (TO_DATE('1992-12-18', 'YYYY-MM-DD'), 'Masculino', TO_DATE('2022-03-20', 'YYYY-MM-DD'), 'SC', 5);
+
+INSERT INTO T_VGS_CASOS (dat_nasc_paciente, des_genero_paciente, dat_diagnostico, des_estado_diagnostico, id_doenca)
+    VALUES (TO_DATE('1988-04-05', 'YYYY-MM-DD'), 'Feminino', TO_DATE('2022-03-25', 'YYYY-MM-DD'), 'PR', 6);
+
+INSERT INTO T_VGS_CASOS (dat_nasc_paciente, des_genero_paciente, dat_diagnostico, des_estado_diagnostico, id_doenca)
+    VALUES (TO_DATE('1997-09-12', 'YYYY-MM-DD'), 'Masculino', TO_DATE('2022-03-30', 'YYYY-MM-DD'), 'BA', 7);
+
+INSERT INTO T_VGS_CASOS (dat_nasc_paciente, des_genero_paciente, dat_diagnostico, des_estado_diagnostico, id_doenca)
+    VALUES (TO_DATE('1983-01-30', 'YYYY-MM-DD'), 'Feminino', TO_DATE('2022-04-05', 'YYYY-MM-DD'), 'GO', 8);
+
+INSERT INTO T_VGS_CASOS (dat_nasc_paciente, des_genero_paciente, dat_diagnostico, des_estado_diagnostico, id_doenca)
+    VALUES (TO_DATE('1995-07-08', 'YYYY-MM-DD'), 'Masculino', TO_DATE('2022-04-10', 'YYYY-MM-DD'), 'MS', 9);
+
+INSERT INTO T_VGS_CASOS (dat_nasc_paciente, des_genero_paciente, dat_diagnostico, des_estado_diagnostico, id_doenca)
+    VALUES (TO_DATE('1986-11-22', 'YYYY-MM-DD'), 'Feminino', TO_DATE('2022-04-15', 'YYYY-MM-DD'), 'MT', 10);
 
 -- Commit para efetivar as insercoes na tabela
 COMMIT;
